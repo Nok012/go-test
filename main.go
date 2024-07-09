53bdee313079955bee100d6dc81cfe039ce08888
Nok012 <dlnnok2545@gmail.com>
2024-07-09 20:15:16 +0700

    add struct AirQualityReading and create function parseReadings

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type AirQualityReading struct {
+	SensorID  string    `json:"sensor_id"`
+	Timestamp time.Time `json:"timestamp"`
+	PM25      float64   `json:"pm25"`
+	CO2       float64   `json:"co2"`
+}
+
+func parseReadings(data []byte) ([]AirQualityReading, error) {
+
+	var airQualityReading []AirQualityReading
+	err := json.Unmarshal(data, &airQualityReading)
+	if err != nil {
+		return nil, err
+	}
+
+	return airQualityReading, nil
+}
